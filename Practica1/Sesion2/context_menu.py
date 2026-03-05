9ec94f6cfc18d1e250bf723642469c09b5a6a0ee
Rafael Galán <43375283+i72gavir@users.noreply.github.com>
2026-03-05 13:13:10 +0100

    Add files via upload

---
+++ Practica1/Sesion2/context_menu.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+"""
+Created on Tue Feb 17 12:50:54 2026
+
+@author: i72gavir
+"""
+
+import sys
+from PyQt5.QtWidgets import QMainWindow, qApp, QMenu, QApplication
+
+
+class Example(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Context menu')
+        self.show()
+
+    def contextMenuEvent(self, event):
+        cmenu = QMenu(self)
+
+        newAct = cmenu.addAction("New")
+        openAct = cmenu.addAction("Open")
+        quitAct = cmenu.addAction("Quit")
+        action = cmenu.exec_(self.mapToGlobal(event.pos()))
+
+        if action == quitAct:
+            qApp.quit()
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
---
+++ Practica1/Sesion2/submenu.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+"""
+Created on Tue Feb 17 12:43:25 2026
+
+@author: i72gavir
+"""
+
+import sys
+from PyQt5.QtWidgets import QMainWindow, QAction, QMenu, QApplication
+
+
+class Example(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('File')
+
+        impMenu = QMenu('Import', self)
+        impAct = QAction('Import mail', self)
+        impMenu.addAction(impAct)
+
+        newAct = QAction('New', self)
+
+        fileMenu.addAction(newAct)
+        fileMenu.addMenu(impMenu)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Submenu')
+        self.show()
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
